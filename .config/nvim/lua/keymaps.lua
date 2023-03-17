dae591d1c54c847919c164a98a9bed73a804a542
GoRuGoo <dachanyr8989@gmail.com>
2023-03-17 15:49:51 +0900

    feat: <C-f>が使えなかったので<C-i>に切り替え

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -2,7 +2,7 @@ vim.api.nvim_set_keymap("i","jj","<ESC>",{})
 
 -- Telescopeのキーバインド
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<C-f>', builtin.find_files, {})
+vim.keymap.set('n', '<C-i>', builtin.find_files, {})
 vim.keymap.set('n', '<C-g>', builtin.live_grep, {})
 vim.keymap.set('n', '<C-b>', builtin.buffers, {})
 vim.keymap.set('n', '<C-h>', builtin.help_tags, {})
