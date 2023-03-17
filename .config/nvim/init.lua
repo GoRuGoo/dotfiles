06fd7f24cbed536973ff070156060267b26eb84c
GoRuGoo <dachanyr8989@gmail.com>
2023-03-17 21:54:15 +0900

    delete: 不要な色設定を削除

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,7 +3,6 @@ require('base')
 require('keymaps')
 
 require('pluginsConfig.lineconfig')
-require('pluginsConfig.color')
 require('pluginsConfig.theme')
 require('pluginsConfig.icons')
 require('pluginsConfig.filer')
