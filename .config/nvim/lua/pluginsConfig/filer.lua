19c892ca234593cf95ce6cbb0ccf9abdc0f9111d
GoRuGoo <dachanyr8989@gmail.com>
2023-03-19 20:14:56 +0900

    feat: qでTelescopeから抜けれるように

--- .config/nvim/lua/pluginsConfig/filer.lua
+++ .config/nvim/lua/pluginsConfig/filer.lua
@@ -1,6 +1,13 @@
 -- You don't need to set any of these options.
 -- IMPORTANT!: this is only a showcase of how you can set default options!
 require("telescope").setup {
+  defaults = {
+    mappings = {
+      n = {
+        ["q"] = require('telescope.actions').close 
+      },
+    },
+  },
   extensions = {
     file_browser = {
       theme = "ivy",
