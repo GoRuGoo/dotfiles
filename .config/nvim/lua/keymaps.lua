837ccad34e6bfcbe2a140f2acfaa6bb67c4db0bc
GoRuGoo <dachanyr8989@gmail.com>
2023-03-19 18:06:15 +0900

    fix: keymapにnoremapを設定してなかったので設定した

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -2,8 +2,8 @@ vim.api.nvim_set_keymap("i","jj","<ESC>",{})
 
 -- Telescopeのキーバインド
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<C-i>', builtin.find_files, {})
-vim.keymap.set('n', '<C-g>', builtin.live_grep, {})
-vim.keymap.set('n', '<C-b>', builtin.buffers, {})
-vim.keymap.set('n', '<C-h>', builtin.help_tags, {})
-vim.keymap.set('n', '<C-k>', '<cmd>:Telescope file_browser path=%:p:h select_buffer=true<cr>',{})
+vim.keymap.set('n', '<C-i>', builtin.find_files, { noremap=true })
+vim.keymap.set('n', '<C-g>', builtin.live_grep, { noremap=true })
+vim.keymap.set('n', '<C-b>', builtin.buffers, { noremap=true })
+vim.keymap.set('n', '<C-h>', builtin.help_tags, { noremap=true })
+vim.keymap.set('n', '<C-k>', '<cmd>:Telescope file_browser path=%:p:h select_buffer=true<cr>',{ noremap = true })
