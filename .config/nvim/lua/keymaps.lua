dcfdf92ac7a38ba82a900b624a008c2bc7ee2516
GoRuGoo <dachanyr8989@gmail.com>
2023-03-21 23:03:02 +0900

    feat: ファイル操作系はすべてスペースを挟むように

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -3,12 +3,10 @@ vim.api.nvim_set_keymap("i","jj","<ESC>",{})
 -- Telescopeのキーバインド
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<space><C-i>', builtin.find_files, {})
-vim.keymap.set('n', '<C-g>', builtin.live_grep, { noremap=true })
-vim.keymap.set('n', '<C-b>', builtin.buffers, { noremap=true })
-vim.keymap.set('n', '<C-h>', builtin.help_tags, { noremap=true })
--- 上は普通のfile_browser 下はdotfilesも表示するfile_browser
-vim.keymap.set('n', '<C-k>', '<cmd>:Telescope file_browser path=%:p:h select_buffer=true<cr>',{ noremap = true })
-vim.keymap.set('n', '<space><C-k>', '<cmd>:Telescope file_browser hidden=true<cr>',{ noremap = true })
+vim.keymap.set('n', '<space><C-g>', builtin.live_grep, { noremap=true })
+vim.keymap.set('n', '<space><C-b>', builtin.buffers, { noremap=true })
+vim.keymap.set('n', '<space><C-h>', builtin.help_tags, { noremap=true })
+vim.keymap.set('n', '<space><C-k>', '<cmd>:Telescope file_browser hidden=true path=%:p:h select_buffer=true<cr>',{ noremap = true })
 
 -- 新規タブ
 vim.keymap.set('n', 'te', ':tabedit')
